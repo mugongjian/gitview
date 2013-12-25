8c61a40c5163f8fbd7a1f0167ad1f2978ab97377
mugong <wb-jianpugang@taobao.com>
2013-12-25 18:06:31 +0800

    rm config

--- eye/config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-DB = ("ux.etao.net","workflow", "workflow", "workflow")
-APP_PORT = 8002
-STATIC_PATH = "/home/jpg/Code/gitview/eye/static"
-APP=dict(
-    debug=True,
-    autoescape=None,
-    static_path=STAITC_PATH)
